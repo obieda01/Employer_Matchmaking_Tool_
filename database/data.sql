655d36929131797e69924f883934d18627f87ece
Obieda01 <obieda01@email.franklin.edu>
2017-04-14 09:21:42 -0400

    add database

--- database/data.sql
+++ database/data.sql
@@ -2,8 +2,76 @@
 -- This script contains INSERT statements for populating tables with seed data
 -- *****************************************************************************
 
-BEGIN;
+--Student Ranking SQL Insert Statements below. Deleting all data to reset DB first
+BEGIN transaction;
 
--- INSERT statements go here
+Delete from Employer_Team;
+Delete from Interview_Schedule;
+Delete from Student_Choices;
+Delete from Time_Slot_Rank;
+Delete from Wait_Listed;
+Delete from Internal_Staff;
+Delete from Employer;
+Delete from Student;
+Delete from Login;
+Delete from Event;
+Delete from Language;
+
+Insert into Login (User_Name, Password, User_Role) VALUES ('Jacob','Password','staff');
+Insert into Login (User_Name, Password, User_Role) VALUES ('Ahmad','Password','admin');
+Insert into Login (User_Name, Password, User_Role) VALUES ('Letha','Password','student');
+Insert into Login (User_Name, Password, User_Role) VALUES ('Jean','Password','student');
+Insert into Login (User_Name, Password, User_Role) VALUES ('Mark','Password','student');
+Insert into Login (User_Name, Password, User_Role) VALUES ('Ashley','Password','student');
+Insert into Login (User_Name, Password, User_Role) VALUES ('Rob','Password','student');
+
+SET IDENTITY_INSERT dbo.Employer ON;
+Insert into Employer (Employer_Id, Employer_Name, Number_Of_Teams, Summary) VALUES (1,'Chase', 1,'Test Summary');
+Insert into Employer (Employer_Id, Employer_Name, Number_Of_Teams, Summary) VALUES (2,'FastSwitch', 1,'Test Summary');
+Insert into Employer (Employer_Id, Employer_Name, Number_Of_Teams, Summary) VALUES (3,'IGS', 1,'Test Summary');
+SET IDENTITY_INSERT dbo.Employer OFF;
+
+Insert into Event (Event_Date, Start_Time, End_Time, Lunch_Start, Lunch_End, First_Break_Start, First_Break_End, Second_Break_Start, Second_Break_End, Interview_Length) VALUES ('4/15/2017','4/15/2017 1:00:00 PM', '4/15/2017 5:00:00 PM',null,null,'4/15/2017 3:15:00 PM','4/15/2017 3:30:00 PM',null,null,45);
+Insert into Language (Language_Id, Language) VALUES (0,'Both');
+Insert into Language (Language_Id, Language) VALUES (1,'Java');
+Insert into Language (Language_Id, Language) VALUES (2,'.Net');
+
+SET IDENTITY_INSERT dbo.Student ON;
+Insert into Student (Student_Id,Student_Name, Language_Id, User_Name) VALUES (1,'Letha Lerman',2,'Letha');
+Insert into Student (Student_Id,Student_Name, Language_Id, User_Name) VALUES (2,'Jean Justus',1,'Jean');
+Insert into Student (Student_Id,Student_Name, Language_Id, User_Name) VALUES (3,'Mark Moore',2,'Mark');
+Insert into Student (Student_Id,Student_Name, Language_Id, User_Name) VALUES (4,'Ashley Anderson',1,'Ashley');
+Insert into Student (Student_Id,Student_Name, Language_Id, User_Name) VALUES (5,'Rob Redmond',1,'Rob');
+SET IDENTITY_INSERT dbo.Student OFF;
+
+Insert into Employer_Team (Employer_Id, Team_Id, Event_Date, Language_Id, Start_Time, End_Time, Assigned_Room) VALUES (1,1,'4/15/2017',0,'1:00:00 PM','5:00:00 PM','Test Room A');
+Insert into Employer_Team (Employer_Id, Team_Id, Event_Date, Language_Id, Start_Time, End_Time, Assigned_Room) VALUES (2,1,'4/15/2017',1,'1:00:00 PM','5:00:00 PM','Test Room B');
+Insert into Employer_Team (Employer_Id, Team_Id, Event_Date, Language_Id, Start_Time, End_Time, Assigned_Room) VALUES (3,1,'4/15/2017',2,'1:00:00 PM','5:00:00 PM','Test Room C');
+
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (1,2,1,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (1,3,2,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (2,1,1,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (2,2,2,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (3,3,1,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (3,1,2,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (4,1,1,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (4,3,2,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (5,2,1,'4/15/2017');
+Insert into Student_Choices (Student_Id, Employer_Id, Employer_Rank, Event_Date) VALUES (5,3,2,'4/15/2017');
+
+Insert into Time_Slot_Rank (Event_Date, Start_Time, End_Time, Time_Slot_Rank) VALUES ('4/15/2017','2017-04-15 01:00:00','4/15/2017 1:45:00',5);
+Insert into Time_Slot_Rank (Event_Date, Start_Time, End_Time, Time_Slot_Rank) VALUES ('4/15/2017',' 4/15/2017 1:45:00 PM','4/15/2017 2:30:00 PM',3);
+Insert into Time_Slot_Rank (Event_Date, Start_Time, End_Time, Time_Slot_Rank) VALUES ('4/15/2017','4/15/2017 2:30:00 PM','4/15/2017 3:15:00 PM',1);
+Insert into Time_Slot_Rank (Event_Date, Start_Time, End_Time, Time_Slot_Rank) VALUES ('4/15/2017','4/15/2017 3:30:00 PM','4/15/2017 4:15:00 PM',2);
+Insert into Time_Slot_Rank (Event_Date, Start_Time, End_Time, Time_Slot_Rank) VALUES ('4/15/2017','4/15/2017 4:15:00 PM','4/15/2017 5:00:00 PM',4);
+
+SET IDENTITY_INSERT dbo.Internal_Staff ON;
+Insert into Internal_Staff (Staff_Id, User_Name,Name, Admin_Flag) VALUES (1,'Jacob','Jacob Rutter','n');
+Insert into Internal_Staff (Staff_Id, User_Name,Name, Admin_Flag) VALUES (2,'Ahmad','Ahmad Obiedat','y');
+SET IDENTITY_INSERT dbo.Internal_Staff OFF;
+
+
 
 COMMIT;
+--End of Student Ranking SQL insert statements
+
--- database/schema.sql
+++ database/schema.sql
@@ -2,8 +2,292 @@
 -- This script creates all of the database objects (tables, constraints, etc) for the database
 -- *************************************************************************************************
 
-BEGIN;
+USE [master]
 
--- CREATE statements go here
+DROP DATABASE [FinalCapstone];
 
-COMMIT;
+CREATE DATABASE [FinalCapstone];
+
+use [FinalCapstone];
+
+BEGIN TRANSACTION;
+
+CREATE TABLE [dbo].[Login](
+	[User_Name] varchar(32) NOT NULL, --
+	[Password] varchar(32) NOT NULL, ---
+	[User_Role] [nchar](7) NOT NULL,
+CONSTRAINT [PK_Login] PRIMARY KEY CLUSTERED ([User_Name] ASC));
+
+CREATE TABLE [dbo].[Internal_Staff](
+	[Staff_Id] [int] IDENTITY(1,1) NOT NULL,
+	[User_Name] [nchar](50) NOT NULL,
+	[Name] [nchar](50) NOT NULL,
+	[Admin_Flag] [char](1) NOT NULL,
+ CONSTRAINT [PK_Internal_Staff] PRIMARY KEY CLUSTERED ([Staff_Id] ASC));
+
+CREATE TABLE [dbo].[Employer](
+	[Employer_Id] [int] IDENTITY(1,1) NOT NULL,
+	[Employer_Name] [nvarchar](50) NOT NULL,
+	[Number_Of_Teams] [int] NOT NULL,
+	[Summary] [text] NULL,
+ CONSTRAINT [PK_Employer] PRIMARY KEY CLUSTERED ([Employer_Id] ASC));
+
+CREATE TABLE [dbo].[Interview_Schedule](
+	[Student_Id] [int] NOT NULL,
+	[Employer_Id] [int] NOT NULL,
+	[Team_Id] [int] NOT NULL,
+	[Time_Slot_Rank] [int] NOT NULL,
+	[Event_Date] [date] NOT NULL,
+
+ CONSTRAINT [PK_Interview_Schedule] PRIMARY KEY CLUSTERED 
+ (
+	[Student_Id] ASC,
+	[Employer_Id] ASC,
+	[Team_Id] ASC
+));
+
+CREATE TABLE [dbo].[Employer_Team](
+	[Employer_Id] [int] NOT NULL,
+	[Team_Id] [int] NOT NULL,
+	[Event_Date] [date] NOT NULL,
+	[Language_Id] [int] NOT NULL,
+	[Start_Time] [time](7) NOT NULL,
+	[End_Time] [time](7) NOT NULL,
+	[Assigned_Room] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Employer_Team] PRIMARY KEY CLUSTERED 
+(
+	[Team_Id] ASC,
+	[Employer_Id] ASC
+));
+
+CREATE TABLE [dbo].[Wait_Listed](
+	[Student_Id] [int] NOT NULL,
+	[Employer_Id] [int] NOT NULL,
+	[Event_Date] [date] NOT NULL,
+	[Resolved] [nchar](1) NOT NULL,
+ CONSTRAINT [PK_Wait_Listed] PRIMARY KEY CLUSTERED 
+(
+	[Student_Id] ASC,
+	[Employer_Id] ASC
+));
+
+CREATE TABLE [dbo].[Event](
+	[Event_Date] [date] NOT NULL,
+	[Start_Time] [time](7) NOT NULL,
+	[End_Time] [time](7) NOT NULL,
+	[Lunch_Start] [time](7) NULL,
+	[Lunch_End] [time](7) NULL,
+	[First_Break_Start] [time](7) NULL,
+	[First_Break_End] [time](7) NULL,
+	[Second_Break_Start] [time](7) NULL,
+	[Second_Break_End] [time](7) NULL,
+	[Interview_Length] [int] NOT NULL,
+ CONSTRAINT [PK_Event] PRIMARY KEY CLUSTERED 
+(
+	[Event_Date] ASC
+));
+
+CREATE TABLE [dbo].[Student_Choices](
+	[Student_Id] [int] NOT NULL,
+	[Employer_Id] [int] NOT NULL,
+	[Employer_Rank] [int] NULL,
+	[Event_Date] [date] NOT NULL,
+ CONSTRAINT [PK_Student_Choices] PRIMARY KEY CLUSTERED 
+(
+	[Student_Id] ASC,
+	[Employer_Id] ASC
+));
+
+CREATE TABLE [dbo].[Student](
+	[Student_Id] [int] IDENTITY(1,1) NOT NULL,
+	[Student_Name] [nvarchar](50) NOT NULL,
+	[Language_Id] [int] NOT NULL,
+	[User_Name] [nchar](50) NOT NULL,
+ CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED 
+(
+	[Student_Id] ASC
+));
+
+
+CREATE TABLE [dbo].[Language](
+	[Language_Id] [int] NOT NULL,
+	[Language] [nchar](10) NOT NULL,
+ CONSTRAINT [PK_Language] PRIMARY KEY CLUSTERED 
+(
+	[Language_Id] ASC
+));
+
+
+CREATE TABLE [dbo].[Time_Slot_Rank](
+	[Event_Date] [date] NOT NULL,
+	[Start_Time] [time](7) NOT NULL,
+	[End_Time] [time](7) NOT NULL,
+	[Time_Slot_Rank] [int] NULL,
+ CONSTRAINT [PK_Time_Slot_Rank] PRIMARY KEY CLUSTERED 
+(
+	[Event_Date] ASC,
+	[Start_Time] ASC
+));
+
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Interview_Schedule_Employer]') AND parent_object_id = OBJECT_ID(N'[dbo].[Interview_Schedule]'))
+ALTER TABLE [dbo].[Interview_Schedule]  WITH CHECK ADD  CONSTRAINT [FK_Interview_Schedule_Employer] FOREIGN KEY([Employer_Id])
+REFERENCES [dbo].[Employer] ([Employer_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Interview_Schedule_Employer]') AND parent_object_id = OBJECT_ID(N'[dbo].[Interview_Schedule]'))
+ALTER TABLE [dbo].[Interview_Schedule] CHECK CONSTRAINT [FK_Interview_Schedule_Employer]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Interview_Schedule_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Interview_Schedule]'))
+ALTER TABLE [dbo].[Interview_Schedule]  WITH CHECK ADD  CONSTRAINT [FK_Interview_Schedule_Event] FOREIGN KEY([Event_Date])
+REFERENCES [dbo].[Event] ([Event_Date])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Interview_Schedule_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Interview_Schedule]'))
+ALTER TABLE [dbo].[Interview_Schedule] CHECK CONSTRAINT [FK_Interview_Schedule_Event]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Interview_Schedule_Student]') AND parent_object_id = OBJECT_ID(N'[dbo].[Interview_Schedule]'))
+ALTER TABLE [dbo].[Interview_Schedule]  WITH CHECK ADD  CONSTRAINT [FK_Interview_Schedule_Student] FOREIGN KEY([Student_Id])
+REFERENCES [dbo].[Student] ([Student_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Interview_Schedule_Student]') AND parent_object_id = OBJECT_ID(N'[dbo].[Interview_Schedule]'))
+ALTER TABLE [dbo].[Interview_Schedule] CHECK CONSTRAINT [FK_Interview_Schedule_Student]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employer_Team_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employer_Team]'))
+ALTER TABLE [dbo].[Employer_Team]  WITH CHECK ADD  CONSTRAINT [FK_Employer_Team_Event] FOREIGN KEY([Event_Date])
+REFERENCES [dbo].[Event] ([Event_Date])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employer_Team_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employer_Team]'))
+ALTER TABLE [dbo].[Employer_Team] CHECK CONSTRAINT [FK_Employer_Team_Event]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employer_Team_Language]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employer_Team]'))
+ALTER TABLE [dbo].[Employer_Team]  WITH CHECK ADD  CONSTRAINT [FK_Employer_Team_Language] FOREIGN KEY([Language_Id])
+REFERENCES [dbo].[Language] ([Language_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Employer_Team_Language]') AND parent_object_id = OBJECT_ID(N'[dbo].[Employer_Team]'))
+ALTER TABLE [dbo].[Employer_Team] CHECK CONSTRAINT [FK_Employer_Team_Language]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Wait_Listed_Employer]') AND parent_object_id = OBJECT_ID(N'[dbo].[Wait_Listed]'))
+ALTER TABLE [dbo].[Wait_Listed]  WITH CHECK ADD  CONSTRAINT [FK_Wait_Listed_Employer] FOREIGN KEY([Employer_Id])
+REFERENCES [dbo].[Employer] ([Employer_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Wait_Listed_Employer]') AND parent_object_id = OBJECT_ID(N'[dbo].[Wait_Listed]'))
+ALTER TABLE [dbo].[Wait_Listed] CHECK CONSTRAINT [FK_Wait_Listed_Employer]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Wait_Listed_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Wait_Listed]'))
+ALTER TABLE [dbo].[Wait_Listed]  WITH CHECK ADD  CONSTRAINT [FK_Wait_Listed_Event] FOREIGN KEY([Event_Date])
+REFERENCES [dbo].[Event] ([Event_Date])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Wait_Listed_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Wait_Listed]'))
+ALTER TABLE [dbo].[Wait_Listed] CHECK CONSTRAINT [FK_Wait_Listed_Event]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Wait_Listed_Student]') AND parent_object_id = OBJECT_ID(N'[dbo].[Wait_Listed]'))
+ALTER TABLE [dbo].[Wait_Listed]  WITH CHECK ADD  CONSTRAINT [FK_Wait_Listed_Student] FOREIGN KEY([Student_Id])
+REFERENCES [dbo].[Student] ([Student_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Wait_Listed_Student]') AND parent_object_id = OBJECT_ID(N'[dbo].[Wait_Listed]'))
+ALTER TABLE [dbo].[Wait_Listed] CHECK CONSTRAINT [FK_Wait_Listed_Student]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Choices_Employer]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student_Choices]'))
+ALTER TABLE [dbo].[Student_Choices]  WITH CHECK ADD  CONSTRAINT [FK_Student_Choices_Employer] FOREIGN KEY([Employer_Id])
+REFERENCES [dbo].[Employer] ([Employer_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Choices_Employer]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student_Choices]'))
+ALTER TABLE [dbo].[Student_Choices] CHECK CONSTRAINT [FK_Student_Choices_Employer]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Choices_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student_Choices]'))
+ALTER TABLE [dbo].[Student_Choices]  WITH CHECK ADD  CONSTRAINT [FK_Student_Choices_Event] FOREIGN KEY([Event_Date])
+REFERENCES [dbo].[Event] ([Event_Date])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Choices_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student_Choices]'))
+ALTER TABLE [dbo].[Student_Choices] CHECK CONSTRAINT [FK_Student_Choices_Event]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Choices_Student]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student_Choices]'))
+ALTER TABLE [dbo].[Student_Choices]  WITH CHECK ADD  CONSTRAINT [FK_Student_Choices_Student] FOREIGN KEY([Student_Id])
+REFERENCES [dbo].[Student] ([Student_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Choices_Student]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student_Choices]'))
+ALTER TABLE [dbo].[Student_Choices] CHECK CONSTRAINT [FK_Student_Choices_Student]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Language]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student]'))
+ALTER TABLE [dbo].[Student]  WITH CHECK ADD  CONSTRAINT [FK_Student_Language] FOREIGN KEY([Language_Id])
+REFERENCES [dbo].[Language] ([Language_Id])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Language]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student]'))
+ALTER TABLE [dbo].[Student] CHECK CONSTRAINT [FK_Student_Language]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Time_Slot_Rank_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Time_Slot_Rank]'))
+ALTER TABLE [dbo].[Time_Slot_Rank]  WITH CHECK ADD  CONSTRAINT [FK_Time_Slot_Rank_Event] FOREIGN KEY([Event_Date])
+REFERENCES [dbo].[Event] ([Event_Date])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Time_Slot_Rank_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Time_Slot_Rank]'))
+ALTER TABLE [dbo].[Time_Slot_Rank] CHECK CONSTRAINT [FK_Time_Slot_Rank_Event]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Time_Slot_Rank_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Time_Slot_Rank]'))
+ALTER TABLE [dbo].[Time_Slot_Rank]  WITH CHECK ADD  CONSTRAINT [FK_Time_Slot_Rank_Event] FOREIGN KEY([Event_Date])
+REFERENCES [dbo].[Event] ([Event_Date])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Time_Slot_Rank_Event]') AND parent_object_id = OBJECT_ID(N'[dbo].[Time_Slot_Rank]'))
+ALTER TABLE [dbo].[Time_Slot_Rank] CHECK CONSTRAINT [FK_Time_Slot_Rank_Event]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Internal_Staff_Login]') AND parent_object_id = OBJECT_ID(N'[dbo].[Internal_Staff]'))
+ALTER TABLE [dbo].[Internal_Staff]  WITH CHECK ADD CONSTRAINT [FK_Internal_Staff_Login] FOREIGN KEY([User_Name])
+REFERENCES [dbo].[Login] ([User_Name])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Internal_Staff_Login]') AND parent_object_id = OBJECT_ID(N'[dbo].[Internal_Staff]'))
+ALTER TABLE [dbo].[Internal_Staff] CHECK CONSTRAINT [FK_Internal_Staff_Login]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Login]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student]'))
+ALTER TABLE [dbo].[Student]  WITH CHECK ADD CONSTRAINT [FK_Student_Login] FOREIGN KEY([User_Name])
+REFERENCES [dbo].[Login] ([User_Name])
+GO
+
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Student_Login]') AND parent_object_id = OBJECT_ID(N'[dbo].[Student]'))
+ALTER TABLE [dbo].[Student] CHECK CONSTRAINT [FK_Student_Login]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[Admin_Yes_or_No]') AND parent_object_id = OBJECT_ID(N'[dbo].[Internal_Staff]'))
+ALTER TABLE [dbo].[Internal_Staff]  WITH CHECK ADD  CONSTRAINT [Admin_Yes_or_No] CHECK  (([Admin_Flag]='Y' OR [Admin_Flag]='N'))
+GO
+
+IF  EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[Admin_Yes_or_No]') AND parent_object_id = OBJECT_ID(N'[dbo].[Internal_Staff]'))
+ALTER TABLE [dbo].[Internal_Staff] CHECK CONSTRAINT [Admin_Yes_or_No]
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[Role_is_Appropriate]') AND parent_object_id = OBJECT_ID(N'[dbo].[Login]'))
+ALTER TABLE [dbo].[Login]  WITH CHECK ADD  CONSTRAINT [Role_is_Appropriate] CHECK  (([User_Role]='Student' OR [User_Role]='Admin' OR [User_Role]='Staff'))
+GO
+
+IF  EXISTS (SELECT * FROM sys.check_constraints WHERE object_id = OBJECT_ID(N'[dbo].[Role_is_Appropriate]') AND parent_object_id = OBJECT_ID(N'[dbo].[Login]'))
+ALTER TABLE [dbo].[Login] CHECK CONSTRAINT [Role_is_Appropriate]
+GO
+
+Commit;

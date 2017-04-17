cfa49e4556dc7401a1936822fba48fb270aed843
Obieda01 <obieda01@email.franklin.edu>
2017-04-17 11:55:59 -0400

    pull

--- Capstone.Web.Tests/Controllers/AdminControllerTests.cs
+++ Capstone.Web.Tests/Controllers/AdminControllerTests.cs
@@ -1,14 +1,26 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Capstone.Web.Tests.Controllers
 {
     [TestClass]
     public class AdminControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestMethod()]
+        public void AdminController_AdminHomeAction_ReturnAboutView()
         {
+            //AdminController controller = new AdminController();
+
+            //ViewResult result = controller.About() as ViewResult;
+
+            //Assert.IsNotNull(result);
+            //Assert.AreEqual("AdminHome", result.ViewName);
         }
     }
 }
